63b12ee3014ca4ada45d43666c8d5e21d44f1201
christianhst <christian.host.madsen@gmail.com>
2025-11-24 11:40:31 +0100

    refactor: moved constants to separate file

---
+++ exam_project/customer_classification_model/constants.py
@@ -0,0 +1,7 @@
+import datetime
+
+# Constants used:
+current_date = datetime.datetime.now().strftime("%Y_%B_%d")
+data_gold_path = "./artifacts/train_data_gold.csv"
+data_version = "00000"
+experiment_name = current_date
