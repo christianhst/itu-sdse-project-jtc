44c06c23de2d03966178d3e43db50bb3bd0e9ff9
christianhst <christian.host.madsen@gmail.com>
2025-12-02 12:11:42 +0100

    refactor: deploy

---
+++ exam_project/customer_classification_model/modeling/deploy.py
@@ -0,0 +1,28 @@
+import warnings
+
+from mlflow.tracking import MlflowClient
+
+from customer_classification_model.constants import model_name
+from customer_classification_model.mlflow_utils import wait_for_deployment
+
+warnings.filterwarnings("ignore")
+
+
+def deploy_model():
+    model_version = 1
+    client = MlflowClient()
+
+    model_version_details = dict(client.get_model_version(name=model_name, version=model_version))
+    model_status = True
+    if model_version_details["current_stage"] != "Staging":
+        client.transition_model_version_stage(
+            name=model_name, version=model_version, stage="Staging", archive_existing_versions=True
+        )
+        model_status = wait_for_deployment(model_name, model_version, "Staging")
+        print(model_status)
+    else:
+        print("Model already in staging")
+
+
+if __name__ == "__main__":
+    deploy_model()
